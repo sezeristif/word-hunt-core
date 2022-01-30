2f4ecf6a242a6168c0ffcb08f5669fef34ebe333
sezeristif <sezeristif@gmail.com>
2022-01-30 20:57:56 +0300

    fix current user method and add emai validation

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -9,7 +9,7 @@ class Api::V1::BaseController < ApplicationController
   private
 
   def current_user
-    @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
+    @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id]) if doorkeeper_token
   end
 
   def unprocessable_entity_response(exception)
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :questions
 
   validates :email, format: URI::MailTo::EMAIL_REGEXP
+  validates_uniqueness_of :email
 
   def self.authenticate(email, password)
     user = User.find_for_authentication(email: email)
